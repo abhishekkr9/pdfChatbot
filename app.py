f3d6489b8fc26029959b3697d6220894de98bee7
abhishekkr9 <141631048+abhishekkr9@users.noreply.github.com>
2024-02-27 14:37:42 +0530

    LangChain integration with Gemini and Chroma

--- app.py
+++ app.py
@@ -1,77 +1,83 @@
-import streamlit as st
-from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+import streamlit as st
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
-def get_pdf_text(pdf_docs):
-    text = ''
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+def text_to_vectordb():
+    loader = PyPDFDirectoryLoader('new_articles')
+    document = loader.load()
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+    allData = text_splitter.split_documents(document)
 
-def get_vector_store(text_chunk):
-    embeddings = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
-    vector_store = FAISS.from_texts(text_chunk, embedding= embeddings)
-    vector_store.save_local('faiss_index')
+    db = Chroma.from_documents(documents = allData, embedding =  embeddings, persist_directory='db_folder')
+    db.persist()
+    db = None
+    
+    
+def get_data_from_db(query):
+    if not Path("new_articles").exists():
+        text_to_vectordb()
+    else:
+        vector_db = Chroma(persist_directory='db_folder', embedding_function=embeddings)
+        retriever = vector_db.as_retriever(search_kwargs={'k': 2})
 
-def get_conversational_chain():
-    prompt_template='''
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details.
-    Context: {context}?
-    Question: {question}
-    Answer:
-    '''
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    prompt = PromptTemplate(template=prompt_template, input_variables=['context','question'])
-    chain=load_qa_chain(model, chain_type='stuff', prompt=prompt)
-    return chain
+        llm = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True)
+        qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
 
-def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    new_db = FAISS.load_local("faiss_index", embeddings)
-    docs = new_db.similarity_search(user_question)
-    chain = get_conversational_chain()
-    response = chain({"input_documents":docs, "question": user_question}, return_only_outputs=True)
-    st.write("Reply: ", response["output_text"])
+        llm_response = qa_chain.invoke(query)
+        return llm_response
+    
+import os
+
+def save_uploaded_file(uploaded_file, save_directory):
+  if uploaded_file is not None:
+    if not os.path.exists(save_directory):
+      os.makedirs(save_directory)
+    filename = f"{uploaded_file.name}"
+    filepath = os.path.join(save_directory, filename)
+    with open(filepath, "wb") as f:
+      f.write(uploaded_file.read())
+    st.success(f"File '{filename}' saved successfully!")
 
 
 def main():
     st.set_page_config("Chat with multiple PDF")
-    st.header("Chat with PDF using Gemini")
-
-    user_question = st.text_input("Ask a Question from the uploaded PDF")
-
-    if user_question:
-        user_input(user_question)
-
+    st.title('Document Search')
     with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Upload & Proceed"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Extracted")
+
+        uploaded_files = st.file_uploader("Choose files:", accept_multiple_files=True)
+
+        save_directory = "new_articles"
+        if uploaded_files is not None:
+            for uploaded_file in uploaded_files:
+                save_uploaded_file(uploaded_file, save_directory)
+
+
+    with st.form(key='my_form'):
+        input_box = st.text_input('Ask a question related to the document...')
+        try:
+            result = get_data_from_db(input_box)
+        except:
+            pass
+        if not input_box:
+            st.warning('Please enter a question.')
+        st.form_submit_button(label='Submit')
+
+    if input_box:
+        st.write(result['result'])
+        st.caption("### Source")
+        st.caption(result)
 
 if __name__ == "__main__":
     main()
